900c42a969a083c9a1f5ace9da3f0d5c471f84d4
dev900317 <bartosz.zagorski@icloud.com>
2021-03-01 22:28:04 +0100

    create basic game logic

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const data = [
-    {
+const data = [{
         id: 1,
-        imgUrl: 'img/bat.jpg',
-        counter: 2
+        imgUrl: 'img/bat.jpg'
     },
     {
         id: 2,
-        imgUrl: 'img/bear.jpg',
-        counter: 2
+        imgUrl: 'img/bear.jpg'
     },
     {
         id: 3,
-        imgUrl: 'img/cat.jpg',
-        counter: 2
+        imgUrl: 'img/cat.jpg'
     },
     {
         id: 4,
-        imgUrl: 'img/cow.jpg',
-        counter: 2
+        imgUrl: 'img/cow.jpg'
     },
     {
         id: 5,
-        imgUrl: 'img/dog.jpg',
-        counter: 2
+        imgUrl: 'img/dog.jpg'
     },
     {
         id: 6,
-        imgUrl: 'img/elephant.jpg',
-        counter: 2
+        imgUrl: 'img/elephant.jpg'
     },
     {
         id: 7,
@@ -36,14 +29,21 @@ const data = [
     },
     {
         id: 8,
-        imgUrl: 'img/spider.jpg',
-        counter: 2
+        imgUrl: 'img/spider.jpg'
     }
 ];
 
 
 const play = document.querySelector('.btn-reset');
 const svgContainer = document.getElementById('svg');
+const cardContainer = document.querySelector('.cards');
+const imgArray = document.querySelectorAll('.card-img');
+const scoreBoard = document.querySelector('.js-score');
+const cardsArray = document.querySelectorAll('.card');
+
+let counter = 0;
+let moves = []
+let score = 0;
 
 const animItem = bodymovin.loadAnimation({
     wrapper: svgContainer,
@@ -59,5 +59,75 @@ play.addEventListener('click', () => {
 })
 
 // animItem.addEventListener('complete', () => {
-//     svgContainer.classList.add('hide');
-// })
+    //     svgContainer.classList.add('hide');
+    // })
+
+
+    // console.log(shuffledData);
+
+    const fillCards = () => {
+        let shuffledData = [...data, ...data]
+        shuffledData = shuffledData.sort(() => Math.random() - 0.5);
+
+        for (let i = 0; i < shuffledData.length; i++) {
+            imgArray[i].src = shuffledData[i].imgUrl;
+            imgArray[i].closest('.card').dataset.id = shuffledData[i].id;
+        }
+
+        cardsArray.forEach(el => el.addEventListener('click', cardClick));
+}
+
+const cardClick = (e) => {
+    console.log(e.currentTarget);
+        // check if user click only 2 times - not more
+        if(moves.length > 1) {
+            return;
+        }
+
+        let currentCard = e.currentTarget;
+        currentCard.querySelector('.card-overlay').classList.add('open');
+        if(currentCard !== moves[0]) {
+            moves.push(currentCard);
+            moveChecker(moves);
+        }
+    }
+
+
+const moveChecker = (arr) => {
+    let [firstMove, secondMove] = arr;
+
+    if(firstMove && secondMove) {
+        if(firstMove.dataset.id === secondMove.dataset.id && !firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
+            score++;
+            scoreBoard.textContent = score;
+            firstMove.cursor = 'default';
+            firstMove.classList.add('scored');
+            firstMove.removeEventListener('click', cardClick);
+            secondMove.cursor  = 'default';
+            secondMove.classList.add('scored');
+            secondMove.removeEventListener('click', cardClick);
+
+            moves = [];
+        }
+        else if(!firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
+            setTimeout(() => {
+                firstMove.querySelector('.card-overlay').classList.remove('open');
+                secondMove.querySelector('.card-overlay').classList.remove('open');
+                moves = [];
+            }, 1000)
+        }
+    } else {
+        return;
+    }
+
+}
+
+const init = () => {
+    fillCards();
+}
+
+
+
+init();
+
+console.log(cardsArray)
