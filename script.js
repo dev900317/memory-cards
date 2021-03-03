94cfacc55ce745a6437b7881f97506fc0a394f58
dev900317 <bartosz.zagorski@icloud.com>
2021-03-03 22:29:00 +0100

    add restart game and timer function

--- script.js
+++ script.js
@@ -38,11 +38,15 @@ const svgContainer = document.getElementById('svg');
 const cardContainer = document.querySelector('.cards');
 const imgArray = document.querySelectorAll('.card-img');
 const scoreBoard = document.querySelector('.js-score');
+const timeBoard = document.querySelector('.js-time');
 const cardsArray = document.querySelectorAll('.card');
 
 let counter = 0;
 let moves = []
 let score = 0;
+
+let sec = 0;
+let timeCounter;
 
 const animItem = bodymovin.loadAnimation({
     wrapper: svgContainer,
@@ -57,66 +61,80 @@ play.addEventListener('click', () => {
 })
 
 
-    const fillCards = () => {
-        let shuffledData = [...data, ...data]
-        shuffledData = shuffledData.sort(() => Math.random() - 0.5);
+const timerStartFunction = () => {
+    clearInterval(timeCounter);
+    sec = 0;
+    timeCounter = setInterval(() =>{
+        timeBoard.textContent = sec + 's';
+        sec++;
+    }, 1000);
+}
 
-        for (let i = 0; i < shuffledData.length; i++) {
-            imgArray[i].src = shuffledData[i].imgUrl;
-            imgArray[i].closest('.card').dataset.id = shuffledData[i].id;
-        }
-        cardsArray.forEach(el => el.addEventListener('click', cardClick));
+const resetBoard = () => {
+    moves = [];
+    // sec = 0;
+    cardsArray.forEach(e => e.classList.remove('scored'))
+}
+
+
+const fillCards = () => {
+    let shuffledData = [...data, ...data]
+    shuffledData = shuffledData.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < shuffledData.length; i++) {
+        imgArray[i].src = shuffledData[i].imgUrl;
+        imgArray[i].closest('.card').dataset.id = shuffledData[i].id;
+    }
+
+    cardContainer.classList.remove('hidden');
+    cardsArray.forEach(el => el.addEventListener('click', cardClick));
 }
 
 const cardClick = (e) => {
-    console.log(e.currentTarget);
-        // check if user click only 2 times - not more
-        if(moves.length > 1) {
-            return;
-        }
-
-        let currentCard = e.currentTarget;
-        currentCard.querySelector('.card-overlay').classList.add('open');
-        if(currentCard !== moves[0]) {
-            moves.push(currentCard);
-            moveChecker(moves);
-        }
+    // check if user click only 2 times - not more
+    if (moves.length > 1) {
+        return;
     }
+
+    let currentCard = e.currentTarget;
+    currentCard.querySelector('.card-overlay').classList.add('open');
+    if (currentCard !== moves[0]) {
+        moves.push(currentCard);
+        moveChecker(moves);
+    }
+}
 
 
 const moveChecker = (arr) => {
     let [firstMove, secondMove] = arr;
 
-    if(firstMove && secondMove) {
-        if(firstMove.dataset.id === secondMove.dataset.id && !firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
+    if (firstMove && secondMove) {
+        if (firstMove.dataset.id === secondMove.dataset.id && !firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
             score++;
             scoreBoard.textContent = score;
-            firstMove.cursor = 'default';
             firstMove.classList.add('scored');
             firstMove.removeEventListener('click', cardClick);
-            secondMove.cursor  = 'default';
             secondMove.classList.add('scored');
             secondMove.removeEventListener('click', cardClick);
 
             moves = [];
-        }
-        else if(!firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
+        } else if (!firstMove.classList.contains('scored') && !secondMove.classList.contains('scored')) {
             setTimeout(() => {
                 firstMove.querySelector('.card-overlay').classList.remove('open');
                 secondMove.querySelector('.card-overlay').classList.remove('open');
                 moves = [];
-            }, 1000)
+            }, 400)
         }
     } else {
         return;
     }
-
 }
 
 const init = () => {
+    resetBoard();
+    timerStartFunction();
     fillCards();
 }
 
-init();
 
-console.log(cardsArray)
+play.addEventListener('click', init);
